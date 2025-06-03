27a314c6fb1b6683213ba522710cb7d8c538f0c4
Sakin <sakin@Sakins-MacBook-Air.local>
2025-06-03 21:48:45 +0600

    first commit

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+import { doctors } from "../assets/assets";
+
+export const AppContext = createContext();
+
+const currencySymbol = '৳';
+
+const AppContextProvider = (props) => {
+  const value = {
+    doctors,
+    currencySymbol
+  };
+
+  return (
+    <AppContext.Provider value={value}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
